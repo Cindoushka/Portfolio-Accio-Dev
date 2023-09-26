1354fc017aecaf91159101c6c11a36ff648af2ef
Cindoushka <cindie-baj@live.fr>
2023-09-26 16:58:39 +0200

    adding date and hour on the iphone on homepage

---
+++ montre.js
@@ -0,0 +1,32 @@
+function afficherHeure() {
+    const elementHeure = document.getElementById('heure');
+    const maintenant = new Date();
+    const heures = maintenant.getHours().toString().padStart(2, '0'); // Format HH
+    const minutes = maintenant.getMinutes().toString().padStart(2, '0'); // Format MM
+
+    const heureActuelle = `${heures}:${minutes}`;
+    elementHeure.textContent = heureActuelle;
+}
+
+// Mettez à jour l'heure toutes les secondes (1000 millisecondes)
+setInterval(afficherHeure, 60000);
+
+// Appelez la fonction une fois au chargement de la page pour afficher l'heure initiale
+afficherHeure();
+
+function afficherDate() {
+    const elementDate = document.getElementById('date');
+    const joursSemaine = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+    const mois = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+
+    const maintenant = new Date();
+    const jourSemaine = joursSemaine[maintenant.getDay()];
+    const jour = maintenant.getDate();
+    const moisActuel = mois[maintenant.getMonth()];
+
+    const dateActuelle = `${jourSemaine} ${jour} ${moisActuel}`;
+    elementDate.textContent = dateActuelle;
+}
+
+// Appelez la fonction une fois au chargement de la page pour afficher la date initiale
+afficherDate();
